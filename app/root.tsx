70308c8d76fa0903b96f17ce51641abac89c0866
mateombar <mateomontabaron@gmail.com>
2024-12-18 11:30:35 +0100

    Search get request

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
     useLoaderData,
     useNavigation,
 } from "@remix-run/react";
-import type {LinksFunction} from "@remix-run/node";
+import type {LinksFunction, LoaderFunctionArgs} from "@remix-run/node";
 import {json, redirect} from "@remix-run/node";
 import appStylesHref from "./app.css?url";
 
@@ -19,11 +19,14 @@ export const links: LinksFunction = () => [
     {rel: "stylesheet", href: appStylesHref},
 ];
 
-export const loader = async () => {
-    const contacts = await getContacts();
-    return json({contacts});
+export const loader = async ({
+                                 request,
+                             }: LoaderFunctionArgs) => {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return json({ contacts });
 };
-
 export const action = async () => {
     const contact = await createEmptyContact();
     return redirect(`/contacts/${contact.id}/edit`);
